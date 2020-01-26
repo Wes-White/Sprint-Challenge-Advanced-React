26eeff096b00cdf9235d0e95f4694caa85c62067
Wes-White <wesleyrwhite86@gmail.com>
2020-01-25 21:13:18 -1000

    rendering data, first test passed

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import playerData from "../../data.js";
+import { render } from "@testing-library/react";
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+test("Player data is present", () => {
+  expect(playerData.data[0].name).toBe("Alex Morgan");
+  expect(playerData.data[0].country).toBe("United States");
+  expect(playerData.data[0].searches).toBe(100);
+});
+
+test("App renders without crashing", () => {
+  render(<App />);
 });
--- client/src/components/PlayersList.js
+++ client/src/components/PlayersList.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const PlayersList = props => {
-  return <div></div>;
+  return (
+    <div>
+      {props.data.map(player => {
+        return (
+          <>
+            <h2>{player.name}</h2>
+            <p>{player.country}</p>
+            <p>{player.searches}</p>
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PlayersList;
