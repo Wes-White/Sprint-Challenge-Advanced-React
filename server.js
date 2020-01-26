d73391d2d286b42e01e92f540c33357dc7e82eb4
Wes-White <wesleyrwhite86@gmail.com>
2020-01-25 20:54:56 -1000

    Created new branch, cra, useEffect pulling in data

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const port = 5000;
 const app = express();
-const { data } = require('./data');
+const { data } = require("./data");
 
 app.use(bodyParser.json());
 
@@ -11,7 +11,7 @@ app.use(cors());
 
 const players = data.map((player, index) => ({ ...player, id: index }));
 
-app.get('/api/players', (req, res) => {
+app.get("/api/players", (req, res) => {
   res.send(players);
 });
 
